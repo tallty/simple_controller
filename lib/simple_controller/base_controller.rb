81027e5e24887b62a870a294727151b5f121389f
Ivan Lan <lanyifan@tallty.com>
2021-01-10 17:24:28 +0800

    Add import_headers api

--- lib/simple_controller/base_controller.rb
+++ lib/simple_controller/base_controller.rb
@@ -46,6 +46,10 @@ class SimpleController::BaseController < ::InheritedResources::Base
     }
   end
 
+  def import_headers
+    render json: { headers: importable_class.import_instance.import_headers }
+  end
+
   def import
     xlsx_file = params[:file] || importable_class.import_excel_klass.new(params[:uid])
     response = importable_class.import_xlsx(xlsx_file, collection, **params.to_unsafe_h.symbolize_keys)
